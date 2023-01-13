a3e4d7b369373e830c823e433db7d22e683348ba
Ivan Pizarroso <ivnp31@gmail.com>
2023-01-13 14:54:44 +0100

    proyect 1

---
+++ src/proyecto1/Main.java
@@ -0,0 +1,34 @@
+package proyecto1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Tpos de Datos Primitivos
+
+        // Enteros
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+
+        // Punto Flotante
+        float number5 = 5f;
+        double number6 = 6d;
+
+        // Caracter
+        char letter1 = 'a';
+
+        // Booleano
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        // Cadena de Caracteres
+        String string1 = "Hola Mundo";
+
+        System.out.println("Entero: " + number3);
+        System.out.println("Long: " + number4);
+        System.out.println("Float: " + number5);
+        System.out.println("Booleano: " + isTrue);
+        System.out.println("Cadena de caracteres: " + string1);
+    }
+}
